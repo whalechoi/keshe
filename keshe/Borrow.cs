119c913e6bd141cc1ad066b2c8fc6b3b6852d0c2
Whale Choi <cj1369636717@gmail.com>
2020-11-25 00:58:02 +0800

    [0.0.1]基础类

---
+++ keshe/Borrow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keshe
+{
+    class Borrow
+    {
+        private decimal BorrowID = 0m;  /* 借书顺序号 */
+        private Int32 rdID = 0; /* 读者序号 */
+        private Int32 bkID = 0; /* 图书序号 */
+        private Int32 ldContinueTimes = 0;  /* 续借次数（第一次借时，记为0） */
+        private DateTime ldDateOut = default(DateTime); /* 借书日期 */
+        private DateTime ldDateRetPlan = default(DateTime); /* 应还日期 */
+        private DateTime ldDateRetAct = default(DateTime);  /* 实际还书日期 */
+        private Int32 ldOverDay = 0;    /* 超期天数 */
+        private decimal ldOverMoney = 0m;   /* 超期金额（应罚款金额） */
+        private decimal ldPunishMoney = 0m; /* 罚款金额 */
+        private Boolean lsHasReturn = false;    /* 是否已经还书，缺省为0-未还 */
+        private string OperatorLend = "";   /* 借书操作员 */
+        private string OperatorRet = "";    /* 还书操作员 */
+
+        public decimal ks_BorrowID { get => BorrowID; set => BorrowID = value; }
+        public int ks_rdID { get => rdID; set => rdID = value; }
+        public int ks_bkID { get => bkID; set => bkID = value; }
+        public int ks_ldContinueTimes { get => ldContinueTimes; set => ldContinueTimes = value; }
+        public DateTime ks_ldDateOut { get => ldDateOut; set => ldDateOut = value; }
+        public DateTime ks_ldDateRetPlan { get => ldDateRetPlan; set => ldDateRetPlan = value; }
+        public DateTime ks_ldDateRetAct { get => ldDateRetAct; set => ldDateRetAct = value; }
+        public int ks_ldOverDay { get => ldOverDay; set => ldOverDay = value; }
+        public decimal ks_ldOverMoney { get => ldOverMoney; set => ldOverMoney = value; }
+        public decimal ks_ldPunishMoney { get => ldPunishMoney; set => ldPunishMoney = value; }
+        public bool ks_lsHasReturn { get => lsHasReturn; set => lsHasReturn = value; }
+        public string ks_OperatorLend { get => OperatorLend; set => OperatorLend = value; }
+        public string ks_OperatorRet { get => OperatorRet; set => OperatorRet = value; }
+    }
+}
---
+++ keshe/ReaderType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keshe
+{
+    class ReaderType
+    {
+        private Int32 rdType = 0;   /* 读者类别 */
+        private string rdTypeName = ""; /* 读者类别名称 */
+        private Int32 CanLendQty = 0;   /* 可借书数量 */
+        private Int32 CanLendDay = 0;   /* 可借书天数 */
+        private Int32 CanContinueTimes = 0; /* 可续借的次数 */
+        private double PunishRate = 0;  /* 罚款率（元/天） */
+        private Int32 DateValid = 0;    /* 证书有效期（年） */
+
+        public int ks_rdType { get => rdType; set => rdType = value; }
+        public string ks_rdTypeName { get => rdTypeName; set => rdTypeName = value; }
+        public int ks_CanLendQty { get => CanLendQty; set => CanLendQty = value; }
+        public int ks_CanLendDay { get => CanLendDay; set => CanLendDay = value; }
+        public int ks_CanContinueTimes { get => CanContinueTimes; set => CanContinueTimes = value; }
+        public double ks_PunishRate { get => PunishRate; set => PunishRate = value; }
+        public int ks_DateValid { get => DateValid; set => DateValid = value; }
+    }
+}
